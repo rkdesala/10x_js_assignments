28ec5e3675e03e7a5a4904161cffb38d4e66765c
rkdesala <ramakrishnadesala95@gmail.com>
2022-03-05 13:22:56 +0530

    fav_movie assignment completed

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,7 +1,15 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
-    // Write your code here
+  // Write your code here
+  //const movie_array = [];
+  if (operation == "add") {
+    movie_array.push(movie);
+    return movie_array;
+  } else if (operation == "remove") {
+    movie_array.pop();
+    return movie_array;
+  }
 }
-
+const movie_array = [];
 module.exports = favouriteMovie;

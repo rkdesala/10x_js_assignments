256166adea5584c04dd10405b674002a8c83e46d
rkdesala <ramakrishnadesala95@gmail.com>
2022-03-05 10:31:03 +0530

    can_drivee_complete

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,18 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
-}
-
-module.exports = CanDrive;
+    if (hasDrivingLiscence === true && isTired === false && isSober === true) {
+      return "You can drive";
+    } else if (
+      hasDrivingLiscence === false &&
+      isTired === true &&
+      isSober === true
+    ) {
+      return "You cannot drive";
+    } else if (hasDrivingLiscence === true && isSober === false) {
+      return "You shouldn't drive";
+    } else {
+    }
+  }
+  
+  module.exports = CanDrive;
+  

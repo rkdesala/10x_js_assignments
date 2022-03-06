c65e79a3dfda9377b6dd2ca8bcd3b40b4a09b63c
rkdesala <ramakrishnadesala95@gmail.com>
2022-03-06 19:53:44 +0530

    assignment_4_final_submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,11 @@
 function CreateObject(arr) {
-    // Write your code here
+  // Write your code here
+  details = {};
+  for (let i = 0; i < arr.length; i += 2) {
+    details[arr[i]] = arr[i + 1];
+  }
+  return details;
 }
-
+//arr = ["rama", "krishna", "desala", "10x_academy"];
+//console.log(CreateObject(arr));
 module.exports = CreateObject;

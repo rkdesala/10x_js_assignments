13bbb9802826a0e2edfe86e4cd408874f86adba8
rkdesala <ramakrishnadesala95@gmail.com>
2022-03-09 19:56:29 +0530

    final_call_back_assignment

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,5 +1,12 @@
+function just(sume) {
+  return sume;
+}
 function RunCallback(a, b, cb) {
-    // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
+  // Write you code here, you need to add a and b,
+  // pass the result into callback function cb as argument return the result
+  let c = 0;
+  c = a + b;
+  return cb(c);
 }
 
 module.exports = RunCallback;
